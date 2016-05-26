57f06b5cfe0a61f34f4e4e80b67a39038a9a8441
Ron Smits <ron.smits@gmail.com>
2016-05-26 21:49:51 +0200

    werkt, maar niet tevreden

--- src/main/kotlin/mainView.kt
+++ src/main/kotlin/mainView.kt
@@ -1,14 +1,10 @@
-import com.sun.javafx.geom.RoundRectangle2D
-import javafx.geometry.Rectangle2D
 import javafx.geometry.VPos
 import javafx.scene.Group
 import javafx.scene.canvas.Canvas
 import javafx.scene.canvas.GraphicsContext
 import javafx.scene.control.ScrollPane
 import javafx.scene.control.TabPane.TabClosingPolicy.UNAVAILABLE
-import javafx.scene.layout.Background
 import javafx.scene.layout.BorderPane
-import javafx.scene.shape.Rectangle
 import javafx.scene.text.Text
 import tornadofx.*
 
@@ -22,7 +18,7 @@ class mainView : View() {
     val orderTreeView: OrderTreeView by inject()
     val orderTableView: OrderSummaryView by inject()
     val orderGraphView: OrderGraphView by inject()
-    val testView : TestView by inject()
+    val testView: TestView by inject()
 
 
     init {
@@ -50,7 +46,7 @@ class mainView : View() {
                                 orderTreeView.updateTreeTable(order.order)
                                 orderTableView.updateTable(order)
                                 orderGraphView.updateGraph(order)
-                                buildmap(order)
+                                testView.updateGraph(order)
                             }
                         }
                     }
@@ -64,6 +60,20 @@ class mainView : View() {
                     tab("test", testView.root) { tabClosingPolicy = UNAVAILABLE }
                 }
             }
+        }
+    }
+
+
+}
+
+
+class TestView : View() {
+    override val root = ScrollPane()
+    val canvas = Canvas(800.0, 600.0)
+
+    init {
+        with(root) {
+            content = canvas
         }
     }
 
@@ -89,6 +99,33 @@ class mainView : View() {
         }
         println("total length will be $totallength")
 
+        val horizontalGap = 50.0
+        val verticalGap = 75.0
+        val center = horizontalGap+totallength/2
+        drawmap.entries.forEach {
+            println(it)
+            val currentRow = it.key
+            val rowlength = calculateRowLength(it.value)
+
+            it.value.forEach {
+                val index = drawmap[currentRow]?.indexOf(it) ?: 0
+                var y = currentRow * verticalGap
+                if (y==0.0) y=20.0
+                var x = center - rowlength/2
+                if (index > 0) {
+                    val leftbox = (drawmap[currentRow]?.get(index - 1) as Box)
+                    x = leftbox.textLength + leftbox.xPos + 6 + horizontalGap
+                }
+                it.draw(x, y, canvas.graphicsContext2D).connect(canvas.graphicsContext2D)
+            }
+        }
+    }
+
+    private fun calculateRowLength(value: MutableList<Box>, gap: Double=50.0): Double {
+        var totallength:Double=0.0
+        value.forEach { totallength+=it.textLength+6+gap }
+        totallength-gap
+        return totallength
     }
 
     private fun recurseDrawMap(row: Int, order: OrderPart, connectTo: Box? = null) {
@@ -100,48 +137,77 @@ class mainView : View() {
         }
         order.orders.forEach { recurseDrawMap(row + 1, it, element) }
     }
-}
 
-class TestView : View() {
-    override val root = ScrollPane()
+    fun updateGraph(order: Order) {
+        drawmap.clear()
+        canvas.graphicsContext2D.clearRect(0.0, 0.0, canvas.width, canvas.height)
+        buildmap(order)
 
-    init {
-        with(root){
-
-            val canvas = Canvas(800.0,600.0)
-            content=canvas
-            val box = Box("first", 1)
-                    box.draw(gc=canvas.graphicsContext2D)
-            Box("second", 3, connectTo = box).draw(80.0, 80.0, gc=canvas.graphicsContext2D)
-            Box("third", 3, box).draw(40.0, 80.0, canvas.graphicsContext2D)
-
-        }
     }
 }
 
-class Box(val name: String, val amount: Int, val connectTo: Box? = null) : Group(){
-    val textfield = Text(name)
-    val textLength = Math.ceil(textfield.layoutBounds.width)
+class Box(val name: String, val amount: Int, val parent: Box? = null) : Group() {
+    val textfield = Text("$name - $amount")
+    val textLength: Double
+        get() = Math.ceil(textfield.layoutBounds.width)
+    val textHeight: Double
+        get() = Math.ceil(textfield.layoutBounds.height)
     var xPos: Double = 10.0
     var yPos: Double = 10.0
 
-    fun connectPoint() :Pair<Double, Double>{
-        return Pair(xPos+(textfield.layoutBounds.width+6)/2, yPos+textfield.layoutBounds.height+6)
+    private val kids= mutableListOf<Box>()
+
+    init {
+        if(parent !=null) {
+            parent.kids.add(this)
+        }
+    }
+    /**
+     * Return the bottom center of the box. This can be used to draw lines between boxes
+     * in the returned Pair <code>first</code> is the x coordinate, <code>second</code> is the y coordinate
+     */
+    fun connectPoint(): Pair<Double, Double> {
+        return Pair(xPos + (textLength + 6) / 2, yPos + textHeight + 6)
     }
 
-    fun draw(x: Double=10.0, y: Double=10.0, gc: GraphicsContext) {
-        xPos=x
-        yPos=y
+    fun draw(x: Double = 10.0, y: Double = 10.0, gc: GraphicsContext): Box {
+
+        xPos = x
+        yPos = y
+
         with(gc) {
             textBaseline = VPos.TOP
-            strokeRoundRect(x, y, textLength+6, textfield.layoutBounds.height+6, 10.0, 10.0)
-            fillText(textfield.text, x+3, y+3)
-            if(connectTo!=null){
+            strokeRoundRect(x, y, textLength + 6, textHeight + 6, 10.0, 10.0)
+            fillText(textfield.text, x + 3, y + 3)
+            if(parent?.kids?.size==1) {
+               // parent?.redraw(x,  gc)
+            }
+        }
+        return this
+    }
+
+    private fun redraw(newX: Double, gc: GraphicsContext) {
+        clear(gc)
+        draw(newX, yPos, gc)
+//        kids.forEach { it.redraw(newX, gc) }
+    }
+    private fun clear(gc: GraphicsContext): Box {
+        gc.clearRect(xPos, yPos, textLength+6, textHeight+7)
+        return this
+    }
+
+    fun connect(gc: GraphicsContext): Box {
+        with (gc) {
+            if (parent != null) {
                 beginPath()
-                moveTo(x+(textLength+6)/2, y)
-                lineTo(connectTo.connectPoint().first, connectTo.connectPoint().second)
+                var xSource : Double = 0.0
+                var ySource : Double = 0.0
+                    xSource = xPos + (textLength + 6) / 2
+                moveTo(xSource, yPos)
+                lineTo(parent.connectPoint().first, parent.connectPoint().second)
                 stroke()
             }
         }
+        return this
     }
 }
